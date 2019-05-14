8531df2f10ecfeb27ddf0d1ccf0b41bbce6a27b5
petern160 <peternguyen160@gmail.com>
2019-05-14 18:25:31 +0000

    completed problems 1-6

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+total_revenue = Store.sum(:annual_revenue)
+average_revenue = Store.average(:annual_revenue)
+one_million = Store.where("annual_revenue > 1000000").count
+
+puts total_revenue
+puts one_million
+puts average_revenue
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,6 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Peter", last_name: "Cool", hourly_rate: 200)
+@store3.employees.create(first_name: "Kanye", last_name: "West", hourly_rate: 62)
